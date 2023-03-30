b01039091deda97e05cd9eb0cca59fee535b013b
ayatomato <mukai.ayato@gmail.com>
2023-03-30 20:08:24 +0900

    PHP3

--- sample.php
+++ sample.php
@@ -1,7 +1,16 @@
 <?php
-$age =20;
-$test ="テスト";
-echo $age;
-echo "\n";
-echo $test;
+$tan=[1,2,3];
+$nizi=[
+    ['松下',29,'男性'],
+    ['高田',25,'男性'],
+    ['岡田',25,'女性']
+];
+$ren =[
+    'name'=>'田中',
+    'age'=>24,
+    'codelang'=>'HTML'
+];
+echo $tan[0]."\n";
+echo '私の名前は'.$nizi[0][0],'です。','年齢は'.$nizi[0][1],'歳'.$nizi[0][2],'です。' ."\n";
+echo $ren['name'];
 ?>
